c9df0e305ab8f3729a5892fb56102674df164f6c
andresperes33 <andresperesdev@gmail.com>
2024-08-03 11:51:22 -0300

    corrigindo numeros

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function clicou() {
 
-    const min = Math.ceil( document.querySelector(".min-input").value)
-    const max = Math.floor( document.querySelector(".max-input").value)
-   let resultado = Math.floor(Math.random()*(max-min +1))+min
-   alert(resultado)
+    const min = Math.ceil(document.querySelector(".min-input").value)
+    const max = Math.floor(document.querySelector(".max-input").value)
 
-    console.log(min, max)
+    if (max >min) {
+
+        let resultado = Math.floor(Math.random() * (max - min + 1)) + min
+    alert(resultado)
+       
+    }
+    else { 
+        alert('O Valor maximo tem que ser maior que o valor minimo')
+
+
+    }
+   
 }
+
+
